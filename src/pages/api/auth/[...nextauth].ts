fdda349062e1747047aaffea34ba1ee091525d91
VitiHugo <vitork42@gmail.com>
2021-08-02 13:13:19 -0300

    :sparkles: Adding connection with FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { fauna } from '../../../services/fauna';
+import { query as q} from 'faunadb';
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
 
 export default NextAuth({
   providers: [
@@ -8,5 +12,40 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+          
+        );
+        return true
+      }
+      catch {
+        return false;
+      }
+    }
+  }
 })
